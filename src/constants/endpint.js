aaeca75712ddb728eccf5a1c11e6964200fdf14d
Debiprasad.Dash <Debiprasad.Dash@navihealth.com>
2023-04-16 22:06:24 +0530

    :)

--- src/constants/endpint.js
+++ src/constants/endpint.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyAwsXb1kJQx1TyOVTt3KiJxTkNAjF5gdiI";
+const GOOGLE_API_KEY = "****";
 export const POPULAR_VIDEO_URL = "https://content-youtube.googleapis.com/youtube/v3/videos?regionCode=US&part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&key="+GOOGLE_API_KEY;
 export const YOUTUBE_SEARCH_API = "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q="
